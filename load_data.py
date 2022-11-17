98d940a10c46e219eadaa9f357050f06682f9dfc
Kevin Abrahamsson <kevin.abrahamsson@iths.se>
2022-11-17 13:21:51 +0100

    fixed bugs

--- load_data.py
+++ load_data.py
@@ -3,5 +3,5 @@ import pandas as pd
 class Load_data:
     @classmethod
     def load(cls):
-        os_data = pd.read_csv("../Data/athlete_events.csv")
+        os_data = pd.read_csv("Data/athlete_events.csv")
         return os_data
